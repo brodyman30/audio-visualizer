2adaa861d310724cbbc0400dc3f542a50d81613c
brodyman30 <108559483+brodyman30@users.noreply.github.com>
2025-11-13 22:44:59 -0600

    Update test-visualizer.js

--- test-visualizer.js
+++ test-visualizer.js
@@ -23,7 +23,7 @@ class TestVisualizer extends HTMLElement {
           width: 100%;
           height: 100%;
           border-radius: 12px;
-          object-fit: cover;
+          object-fit: contain;
         }
 
         .visualizer {
