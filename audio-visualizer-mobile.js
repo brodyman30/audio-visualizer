9d95dc94f4a65037162faebd76a09b9876391490
brodyman30 <108559483+brodyman30@users.noreply.github.com>
2025-12-01 15:29:30 -0600

    Update audio-visualizer-mobile.js

--- audio-visualizer-mobile.js
+++ audio-visualizer-mobile.js
@@ -34,7 +34,7 @@ class AudioVisualizer extends HTMLElement {
           border-radius: 12px;
           object-fit: contain;
           pointer-events: none;
-          box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
+        
         }
         .visualizer-circle {
           position: absolute;
